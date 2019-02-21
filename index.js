5af7dc9d25f8350ca478a49d502490f1546fa4c5
Rowan Taylor <rowan.taylor@colorado.edu>
2019-02-21 01:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function subtract(a, b) {
   var dif = a - b;
   return dif;
 };
+
+function multiply(a, b) {
+  var prod = a * b;
+  return prod;
+};
+
+function divide(a, b) {
+  var div = a / b;
+  return div;
+};
+
